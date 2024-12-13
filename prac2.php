f40b71c990f2068f099c7b012c72047ffec2e751
mutsuki-kano6 <mutsukitest@gmail.com>
2024-12-13 10:55:56 +0900

    Create prac2.php

---
+++ prac2.php
@@ -0,0 +1,13 @@
+<?php
+$month="12月";
+$keiba1="有馬";
+$keiba2="宝塚";
+$keiba3="セントライト";
+if($number="12月"){
+        $race="keiba1";
+}elseif($number="6月"){
+        $race="keiba2";
+}elseif($number="9月"){
+        $race="keiba3";
+}
+print($month."は".$$race."記念です。");
